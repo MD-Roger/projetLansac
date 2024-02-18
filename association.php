aba44d0b3e2204a5e471026124c1e34d317b7451
dvvuuh <dauh_13@live.fr>
2024-02-18 17:03:56 +0100

    create decouvrir,asso.php pages with basic style

---
+++ association.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="./css/styleAsso.css">
+    <title>Découvrir</title>
+</head>
+<body>
+    <header>
+        <div class="header">
+        header ici !
+        </div>
+
+    </header>
+    <main>
+       <section class="mainContent">
+
+        <div class="firstContent">
+        <h2>L'histoire du hameau.</h2>
+        <p>L'église ne comporte que 85 places assises, 
+            et les derniers arrivés doivent se mettre dehors. 
+            Normalement un berger devrait apporter un agneau pour le pastrage afin d'honorer la naissance de l'enfant Jésus".
+        </p>
+        </div>
+
+        <div class="secondContent">
+        <h2>L'histoire du hameau.</h2>
+        <p>L'église ne comporte que 85 places assises, 
+            et les derniers arrivés doivent se mettre dehors. 
+            Normalement un berger devrait apporter un agneau pour le pastrage afin d'honorer la naissance de l'enfant Jésus".
+        </p>
+        </div>
+    
+       </section>
+    </main>
+    <?php include 'footer.html'; ?>
+
+</body>
+</html>
