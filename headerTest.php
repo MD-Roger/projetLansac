5469a5f90f0ce4429e3882f10a85f192e25127ad
dvvuuh <dauh13980@gmail.com>
2024-03-01 12:28:00 +0100

    headerTest fix

--- headerTest.php
+++ headerTest.php
@@ -8,11 +8,10 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Aclonica&family=Poiret+One&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="./headerStyle.css">
+    <link rel="stylesheet" href="./css/headerRStyle.css">
     <!-- <link rel="stylesheet" href="./style.css"> -->
 </head>
 <body>
-
 
 
 <header>
@@ -90,7 +89,7 @@
 
 
             </div>
-            <div class="second testRR">
+            <div class="second">
                 <p class ="description">L'église fédère la population, les anciens et les nouveaux du hameau qui se retrouvent lors de rendez-vous religieux importants comme Noël. Une messe sera célébrée ce 24 décembre, à 19h. Malheureusement la jauge sera limitée à 30 personnes et nous ne pourrons pas accueillir tous les fidèles" déplore Florence Testud, la secrétaire de l'association des Amis de l'Église de Lansac. "Habituellement, cet événement rassemble les gens bien au-delà des 200 habitants du hameau et alentours.  </p>
                 <img class="gallery" src="img/IMG_9040.JPEG" alt="">
 
@@ -101,7 +100,7 @@
                 <p class ="description">C'est le père Claude Gay, de Saint-Étienne du Grès, qui célébrera la messe de Noël. Avec le père Michel Savalli, curé de l'Unité Pastorale Sainte-Marthe de Tarascon, ils viennent régulièrement dans ce lieu officier pour les baptêmes, les mariages, les enterrements. "À la mi-avril, nous célébrons les Rameaux. On fait venir à cette occasion un troupeau de chèvres. Fin mai début juin, une autre messe est donnée suivie du verre de l'amitié. On y déguste les vins du Domaine de Lansac et du Grand Mas de Lansac. C'est très agréable.</p>
             </div>
 
-            <div class="fourth testRR">
+            <div class="fourth">
                 <p class ="description">Il y a également la fête des vendanges après l'été, avec une procession, une charrette décorée de ceps de vigne et de grappes de raisins. Après la messe nous partageons entre voisins et paroissiens un bon petit repas. La première année nous étions 120, puis 170 l'année suivante... Nous faisons également venir des groupes folkloriques qui animent les balèti ou qui donnent des concerts. C'est vraiment très convivial cette vie au hameau" poursuit Florence Testud heureuse de pouvoir assister, malgré le Covid, à la messe de Noël à Notre Dame de L'Assomption.Comme toujours un vin chaud sera servi à l'issue de la cérémonie.</p>
                 <img class="gallery" src="img/IMG_9021.JPEG" alt="" srcset="">
             </div>
--- pageDon.php
+++ pageDon.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/reset.css">
+    
 
     <link rel="stylesheet" href="./css/pageDonStyle.css">
     <title>Evenement</title>
