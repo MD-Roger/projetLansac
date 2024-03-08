8a77c280c3ec54a440031c06b96b6747146da9be
dvvuuh <dauh13980@gmail.com>
2024-03-08 16:11:44 +0100

    adding responsive for mobile

--- event.php
+++ event.php
@@ -27,7 +27,7 @@
     
     <div class="secondContent">
     <h2>Fête des lumières.<br>
-Prochaine date 01.05.2024 <a href="#"><u>Inscrivez-vous ici.</u></a></h2>
+Prochaine date 01.05.2024 <a href="#">Inscrivez-vous ici.</a></h2>
     <p>L'église ne comporte que 85 places assises, 
         et les derniers arrivés doivent se mettre dehors. 
         Normalement un berger devrait apporter un agneau pour le pastrage afin d'honorer la naissance de l'enfant Jésus".
